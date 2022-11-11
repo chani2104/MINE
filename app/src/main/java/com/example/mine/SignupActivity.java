1c8fcfc53b760d92e0440c703dd3a9f72c785f2b
jongchanim <99734417+chani2104@users.noreply.github.com>
2022-11-11 16:22:36 +0900

    (11.11) 회원가입 날짜 추가

--- app/src/main/java/com/example/mine/SignupActivity.java
+++ app/src/main/java/com/example/mine/SignupActivity.java
@@ -2,6 +2,7 @@ package com.example.mine;
 
 import static android.content.ContentValues.TAG;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +10,9 @@ import android.widget.EditText;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +25,14 @@ public class SignupActivity extends AppCompatActivity {
 
     }
 
+    private String getTime(){
+        long timeNow =  System.currentTimeMillis();
+        Date date = new Date(timeNow);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
+
+        return dateFormat.format(date);
+    }
+
     public void UserSaveFirebase(View view){
         EditText nickName = findViewById(R.id.nickname_edit);
         EditText ID = findViewById(R.id.id_edit);
@@ -31,12 +43,15 @@ public class SignupActivity extends AppCompatActivity {
         data.put("닉네임", nickName.getText().toString());
         data.put("아이디", ID.getText().toString());
         data.put("비밀번호", password.getText().toString());
+        data.put("가입날짜", getTime());
 
         db.collection("user_info").document(ID.getText().toString())
                 .set(data)
                 .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot Success"))
                 .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
     }
+
+
 
 
 }
