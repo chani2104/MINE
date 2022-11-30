3d264e5022348f8d7ebeabe908c70aacdb5a76ad
jongchanim <99734417+chani2104@users.noreply.github.com>
2022-12-01 05:13:15 +0900

    사용자 ID값 전송 (manifest 수정 필요)

--- app/src/main/java/com/example/mine/LogInActivity.java
+++ app/src/main/java/com/example/mine/LogInActivity.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,61 +18,65 @@ import java.util.Objects;
 
 public class LogInActivity extends AppCompatActivity {
     EditText login_id, login_pw;
+    Button login_btn;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.user_login);
-    }
-
-    public void onLogInClicked(View view){
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        login_id = findViewById(R.id.login_id);
+        login_pw = findViewById(R.id.login_password);
+        login_btn = findViewById(R.id.login_button);
 
         SharedPreferences sharedPref = getSharedPreferences("sharedPreferences", Context.MODE_PRIVATE);
         String loginID = sharedPref.getString("inputID", null);
         String loginPW = sharedPref.getString("inputPW", null);
 
-
-
         if (loginID != null && loginPW != null) {
-            Intent intent = new Intent(getApplicationContext(), AppLock.class);
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
             intent.putExtra("id", loginID);
             startActivity(intent);
             finish();
-        }
-
-        else if (loginID == null && loginPW == null){
+        } else if (loginID == null && loginPW == null) {
             SharedPreferences.Editor editor = sharedPref.edit();
-
-            db.collection("user_info")
+            login_btn.setOnClickListener(view -> db.collection("user_info")
                     .get()
                     .addOnSuccessListener(task -> {
-                        boolean docFound = false;
-                        Object pw = null;
-                        String doc = "";
-                        for (DocumentSnapshot ds : task.getDocuments()) {
-                            if (ds.getId().equals(login_id.getText().toString())) {
-                                docFound = true;
-                                pw = ds.get("비밀번호");
-                                doc = ds.getId();
+                        if (login_id.getText().toString().isEmpty())
+                            Toast.makeText(this, "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        else if (login_pw.getText().toString().isEmpty())
+                            Toast.makeText(this, "패스워드를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        else {
+                            boolean docFound = false;
+                            Object pw = null;
+                            String doc = "";
+                            for (DocumentSnapshot ds : task.getDocuments()) {
+                                if (ds.getId().equals(login_id.getText().toString())) {
+                                    docFound = true;
+                                    pw = ds.get("비밀번호");
+                                    doc = ds.getId();
+                                }
                             }
-                        }
+                            System.out.println(docFound);
                             if (docFound) {
                                 if (!Objects.requireNonNull(pw).toString().equals(login_pw.getText().toString())) {
                                     Toast.makeText(LogInActivity.this, "비밀번호가 틀립니다.", Toast.LENGTH_LONG).show();
                                 } else if (Objects.requireNonNull(pw).toString().equals(login_pw.getText().toString())) {
-                                    Intent intent = new Intent(getApplicationContext(), AppLock.class);
+                                    Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                                     intent.putExtra("id", doc);
                                     editor.putString("inputID", doc);
+                                    editor.putString("inputPW", pw.toString());
                                     editor.apply();
+                                    Toast.makeText(LogInActivity.this, "로그인에 성공했습니다.", Toast.LENGTH_LONG).show();
                                     startActivity(intent);
                                 }
-                            }
-                            else {
+                            } else {
                                 Toast.makeText(LogInActivity.this, "아이디가 존재하지 않습니다.", Toast.LENGTH_LONG).show();
                             }
-                    });
+                        }
+                    }));
         }
     }
 }
