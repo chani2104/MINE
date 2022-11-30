299ad0458fa918cde2d1965af89a07269bab6a12
jongchanim <99734417+chani2104@users.noreply.github.com>
2022-12-01 04:09:12 +0900

    로그인

--- app/src/main/java/com/example/mine/LogInActivity.java
+++ app/src/main/java/com/example/mine/LogInActivity.java
@@ -6,30 +6,27 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Source;
 
 import java.util.Objects;
 
 public class LogInActivity extends AppCompatActivity {
     EditText login_id, login_pw;
-    Button login_btn;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.user_login);
+    }
 
-        login_id = findViewById(R.id.login_id);
-        login_pw = findViewById(R.id.login_password);
-        login_btn = findViewById(R.id.login_button);
+    public void onLogInClicked(View view){
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -37,31 +34,46 @@ public class LogInActivity extends AppCompatActivity {
         String loginID = sharedPref.getString("inputID", null);
         String loginPW = sharedPref.getString("inputPW", null);
 
+
+
         if (loginID != null && loginPW != null) {
             Intent intent = new Intent(getApplicationContext(), AppLock.class);
             intent.putExtra("id", loginID);
             startActivity(intent);
             finish();
         }
+
         else if (loginID == null && loginPW == null){
-            login_btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    db.collection("user_info")
-                            .get()
-                            .addOnSuccessListener(task -> {
-                                for (DocumentSnapshot ds : task.getDocuments()) {
-                                    if(ds.getId().equals(login_id.getText().toString())) {
-                                        DocumentReference docRef = db.collection("user_info").document(ds.getId());
-                                        if (!Objects.requireNonNull(ds.get("비밀번호")).toString().equals(login_pw.getText().toString())){
+            SharedPreferences.Editor editor = sharedPref.edit();
 
-                                        }
-
-                                    }
+            db.collection("user_info")
+                    .get()
+                    .addOnSuccessListener(task -> {
+                        boolean docFound = false;
+                        Object pw = null;
+                        String doc = "";
+                        for (DocumentSnapshot ds : task.getDocuments()) {
+                            if (ds.getId().equals(login_id.getText().toString())) {
+                                docFound = true;
+                                pw = ds.get("비밀번호");
+                                doc = ds.getId();
+                            }
+                        }
+                            if (docFound) {
+                                if (!Objects.requireNonNull(pw).toString().equals(login_pw.getText().toString())) {
+                                    Toast.makeText(LogInActivity.this, "비밀번호가 틀립니다.", Toast.LENGTH_LONG).show();
+                                } else if (Objects.requireNonNull(pw).toString().equals(login_pw.getText().toString())) {
+                                    Intent intent = new Intent(getApplicationContext(), AppLock.class);
+                                    intent.putExtra("id", doc);
+                                    editor.putString("inputID", doc);
+                                    editor.apply();
+                                    startActivity(intent);
                                 }
-                            });
-                }
-            });
+                            }
+                            else {
+                                Toast.makeText(LogInActivity.this, "아이디가 존재하지 않습니다.", Toast.LENGTH_LONG).show();
+                            }
+                    });
         }
     }
 }
