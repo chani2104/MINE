65bc89cefacb2a3e5750f9d8eec01388fca5aa54
jongchanim <99734417+chani2104@users.noreply.github.com>
2022-12-11 17:07:43 +0900

    Update WritingDiary.java

--- app/src/main/java/com/example/mine/WritingDiary.java
+++ app/src/main/java/com/example/mine/WritingDiary.java
@@ -1,14 +1,28 @@
 package com.example.mine;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WritingDiary extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,19 +30,32 @@ public class WritingDiary extends AppCompatActivity implements View.OnClickListe
     Button deleteDiary;
     Button saveDiary;
     EditText writeDiary;
+    String date;
+    String userInput;
 
+    //시간
+    Instant time;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.user_diary);
+
+        Intent intent = getIntent();
+        date = intent.getStringExtra("localDate");
+
         writingBack = findViewById(R.id.writing_back);
         deleteDiary = findViewById(R.id.delete_Diary);
         saveDiary = findViewById(R.id.save_diary);
         writeDiary = findViewById(R.id.write_diary);
 
-        writingBack.setOnClickListener(this);
+        writingBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
         deleteDiary.setOnClickListener(this);
         saveDiary.setOnClickListener(this);
 
@@ -36,19 +63,35 @@ public class WritingDiary extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View view) {
-        if(view == writingBack){
-            Intent intent = new Intent(this,UserDiary.class);
-            startActivity(intent);
-        }
-        else if(view == deleteDiary){
-            writeDiary.setText("");
-        }
 
-        else if(view == saveDiary){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String lockID = ((LogInActivity) LogInActivity.context_login).doc;
+        System.out.println(lockID);
+        DocumentReference docRef = db.collection("user_photo").document(lockID).collection("photo").document(date);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                Map<String, Object> input = new HashMap<>();
+                if (view == deleteDiary) {
+                    writeDiary.setText("");
+                }
 
 
-        }
+                else if (view == saveDiary) {
+
+                    if(time == null){
+                        time = Instant.now();
+                    }
+                    userInput = writeDiary.getText().toString();
+                    input.put(String.valueOf(time),userInput);
+                    docRef.update(input);
+                }
+            }
+        });
     }
 }
